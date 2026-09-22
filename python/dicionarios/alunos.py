8d07d5f941831ccf454402a4677905f9f8cf3407
Aluno Senac <Aluno@SOBLABDQ45Y541NB.corp.ce.senac.br>
2026-09-22 10:32:06 -0300

    Criação dos primeiros arquivos com python

---
+++ python/dicionarios/alunos.py
@@ -0,0 +1,11 @@
+'''
+Criação: Crie um dicionário chamado alunos contendo 3 alunos como chaves e seus respectivos dicionários internos com nome, idade e curso.
+'''
+
+alunos = {
+    "nome": ["Diogo", "Olavo", "Iego"],
+    "idade": [18],
+    "curso": ["Programador de Sistemas"]
+}
+
+print(alunos["nome"], alunos["idade"], alunos["curso"])
---
+++ python/exercicios-de-logica/desconto.py
@@ -0,0 +1,6 @@
+valor_produto = float(input("Digite o valor do produto: "))
+desconto = int(input("Digite o desconto que deseja dar ao produto: "))
+
+valor_final = valor_produto * (desconto / 100)
+
+print(f"O valor do produto é {valor_produto}, com desconto fica: {valor_final}")
