923af5fe6e1756aeeca2a4c6010acff1376ba408
Aluno Senac <Aluno@SOBLABDQ45Y541NB.corp.ce.senac.br>
2026-09-23 09:51:25 -0300

    Novo commit

--- python/dicionarios/alunos.py
+++ python/dicionarios/alunos.py
@@ -2,10 +2,40 @@
 Criação: Crie um dicionário chamado alunos contendo 3 alunos como chaves e seus respectivos dicionários internos com nome, idade e curso.
 '''
 
+aluno1 = {
+    "nome": "Diogo",
+    "idade": 18,
+    "curso": "Programador de Sistemas"
+}
+
+aluno2 = {
+    "nome": "João",
+    "idade": 19,
+    "curso": "Engenharia de Software"
+}
+
+aluno3 = {
+    "nome": "Pedro",
+    "idade": 20,
+    "curso": "Administração"
+}
+
+alunos = {
+    
+}
+
+print(f"O primeiro aluno é {aluno1}")
+print(f"O segundo aluno é {aluno2}")
+print(f"O terceiro aluno é {aluno3}")
+
+
+"""
+Segunda forma de fazer um dicionário de alunos com listas de valores para cada chave.
+
 alunos = {
     "nome": ["Diogo", "João", "Pedro"],
     "idade": [18],
     "curso": ["Programador de Sistemas"]
 }
 
-print(alunos["nome"], alunos["idade"], alunos["curso"])
+print(alunos["nome"], alunos["idade"], alunos["curso"])"""
---
+++ python/dicionarios/alunosfor.py
@@ -0,0 +1,12 @@
+'''
+Iteração: Utilize um laço for junto com metódo .items() para imprimir cada chave e valor do dicionário alunos no formato: Chave X - Dados Y.
+'''
+
+alunos = {
+    "nome": "Diogo",
+    "idade": 18,
+    "curso": "Programador de Sistemas"
+}
+
+for chave, valor in alunos.items():
+    print(f"Chave: {chave} - Dados: {valor}")
