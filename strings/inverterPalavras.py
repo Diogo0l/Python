7ede464aa39a92d611364382f7091126d65c2462
Aluno Senac <Aluno@SOBLABDQ45Y541NB.corp.ce.senac.br>
2026-09-25 09:54:48 -0300

    Criação de um algoritmo de capitalizar palavras.

--- strings/inverterPalavras.py
+++ strings/inverterPalavras.py
@@ -3,3 +3,9 @@ Inverter ordem das palavras
 - Escreva um algoritmo que inverte a ordem das palavras em uma frase.
 '''
 
+frase = input("Digite uma frase: ")
+
+palavras = frase.split()
+palavras.reverse()
+
+print(" ".join(palavras))
