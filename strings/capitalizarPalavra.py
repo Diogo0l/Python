c244e2dacd7edd4fe770046ac6c6245fb9121b09
Aluno Senac <Aluno@SOBLABDQ45Y541NB.corp.ce.senac.br>
2026-09-25 09:57:49 -0300

    Criação de um algoritmo de capitalizar palavras.

--- strings/capitalizarPalavra.py
+++ strings/capitalizarPalavra.py
@@ -1,9 +1,11 @@
-## 5.Capitalizar primeira letra de cada palavra
-#Escreva um programa que capitalize a primeira letra de cada palavra em uma frase.
-# Algoritimo:
-# - Receber uma frase do usuario
-# - Capitalizar a primeira letra de cada palavra
-# - Mostrar o resultado
+'''
+5.Capitalizar primeira letra de cada palavra
+Escreva um programa que capitalize a primeira letra de cada palavra em uma frase.
+Algoritimo:
+- Receber uma frase do usuario
+- Capitalizar a primeira letra de cada palavra
+- Mostrar o resultado
+'''
 
 frase = input("Digite uma frase: ") 
 frase_formatada = frase.title()
